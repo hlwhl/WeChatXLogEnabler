ceb4bc8d955776b7e745686144c7d7d008ff5686
hlwhl <levi.hao96@gmail.com>
2019-07-23 17:51:47 +0800

    适配输出level

--- app/src/main/java/com/linwei/electronicdream/wechathook/LogHook.kt
+++ app/src/main/java/com/linwei/electronicdream/wechathook/LogHook.kt
@@ -6,6 +6,11 @@ import com.gh0u1l5.wechatmagician.spellbook.interfaces.IXLogHook
 object LogHook : IXLogHook{
     override fun onXLogWrite(level: String, tag: String, msg: String) {
         super.onXLogWrite(level, tag, msg)
-        Log.e("HOOKEDXLOG"+tag,msg)
+        when(level){
+            "INFO" -> Log.i("HOOKEDXLOG "+tag,msg)
+            "ERROR" -> Log.e("HOOKEDXLOG "+tag,msg)
+            "WARNING" -> Log.w("HOOKEDXLOG "+tag,msg)
+            else -> Log.e("HOOKEDXLOG level: " +level + "TAG: " + tag, msg)
+        }
     }
 }
